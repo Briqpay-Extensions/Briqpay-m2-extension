01393d3501231ae606e852e130c85608e9bf11eb
Mats Andersson <mats@briqpay.com>
2024-10-29 10:34:29 +0100

    Briqpay-extension-1-0-10

--- Briqpay/Payments/Setup/UpgradeSchema.php
+++ Briqpay/Payments/Setup/UpgradeSchema.php
@@ -22,8 +22,8 @@ class UpgradeSchema implements UpgradeSchemaInterface
         $setup->startSetup();
         $this->logger->info('UpgradeSchema: start');
 
-        if (version_compare($context->getVersion(), '1.0.10', '<')) {
-            $this->logger->info('UpgradeSchema: version is less than 1.0.10');
+        if (version_compare($context->getVersion(), '1.0.11', '<')) {
+            $this->logger->info('UpgradeSchema: version is less than 1.0.11');
 
             $this->createOrderTableColumns($setup);
             $this->createQuoteTableColumns($setup);
