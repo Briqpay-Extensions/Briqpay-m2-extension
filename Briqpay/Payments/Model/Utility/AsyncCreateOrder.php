20515ce6aa3eeb5fe83473c4c080a70787b0a10c
Mats Andersson <mats@briqpay.com>
2026-03-06 09:55:47 +0100

    Briqpay-extension-1-1-10

--- Briqpay/Payments/Model/Utility/AsyncCreateOrder.php
+++ Briqpay/Payments/Model/Utility/AsyncCreateOrder.php
@@ -22,7 +22,7 @@ class AsyncCreateOrder
     protected $orderRepository;
     protected $logger;
     protected $customerSession;
-     /**
+    /**
      * @var ScopeConfigInterface
      */
     protected $scopeConfig;
@@ -68,7 +68,7 @@ class AsyncCreateOrder
     {
         try {
             $this->logger->info('Received webhook payload: ' . json_encode($sessionData));
-    
+
             // Load quote
             $quote = $this->quoteRepository->get($quoteId);
 
@@ -76,65 +76,65 @@ class AsyncCreateOrder
                 $quote->setData('briqpay_session_id', $sessionData['sessionId']);
                 $this->quoteRepository->save($quote);
             }
-    
+
             if (!$quote->getId()) {
                 throw new LocalizedException(__('Quote not found.'));
             }
-    
+
             // Validate email format
             $billingEmail = $this->extractBillingEmail($quote, $sessionData);
             if (!filter_var($billingEmail, FILTER_VALIDATE_EMAIL)) {
                 throw new LocalizedException(__('Invalid email format for billing address: %1', $billingEmail));
             }
-    
+
             // Extract moduleStatus
             $moduleStatus = $sessionData['moduleStatus'] ?? [];
-    
+
             // Determine order status from moduleStatus
             if (isset($moduleStatus['payment']['orderStatus'])) {
                 $incomingStatus = $moduleStatus['payment']['orderStatus'];
             } else {
                 throw new LocalizedException(__('Order status not found in moduleStatus.'));
             }
-    
+
             // Check if the order has already been processed
             $reservedOrderId = $quote->getReservedOrderId();
             if ($reservedOrderId) {
                 // This is before you attempt to submit the order, so don't look for the order yet.
                 $this->logger->debug('Quote has reserved order ID: ' . $reservedOrderId);
             }
-    
+
             // Set customer information
             $this->setCustomerInformation($quote, $billingEmail);
             $transactions = $sessionData['data']['transactions'] ?? [];
-            $pspMetaData = $session['data']['pspMetadata'] ?? [];
+            $pspMetaData = $sessionData['data']['pspMetadata'] ?? [];
             $pspDisplayName = !empty($transactions) ? $transactions[0]['pspDisplayName'] : '';
             $quote->setData('briqpay_psp_display_name', $pspDisplayName);
-    
+
             // Collect totals and save quote
             $quote->collectTotals()->save();
-    
+
             // Convert quote to order
             $order = $this->quoteManagement->submit($quote);
-    
+
             if (!$order || !$order->getId()) {
                 throw new LocalizedException(__('Order could not be created.'));
             }
-    
+
             // At this point, the order has been created successfully.
             $this->logger->info('Order created successfully with order ID: ' . $order->getId());
-    
+
             // Now, apply the state and status based on incoming status
             if (isset($this->statusMapping[$incomingStatus])) {
                 $state = $this->statusMapping[$incomingStatus]['state'];
                 $status = $this->statusMapping[$incomingStatus]['status'];
-    
+
                 $order->setState($state);
                 $order->setStatus($status);
-    
+
                 // Additional data related to the order
                 $this->setOrderAdditionalData($order, $sessionData);
-    
+
                 // Save order
                 $order->save();
             } else {
@@ -167,8 +167,8 @@ class AsyncCreateOrder
 
     protected function setOrderAdditionalData($order, $sessionData)
     {
-        
-        
+
+
         // Extract transactions
         $transactions = $sessionData['data']['transactions'] ?? [];
         $pspMetaData = $sessionData['data']['pspMetadata'] ?? [];
@@ -178,21 +178,21 @@ class AsyncCreateOrder
         $pspReservationId = !empty($transactions) ? $transactions[0]['reservationId'] : '';
         $briqpaySessionStatus = !empty($transactions) ? $transactions[0]['status'] : '';
         $secondaryReference = $transactions[0]['secondaryReservationId'] ?? '';
-    
+
         // Set PSP display name
         if ($pspDisplayName !== '') {
             $order->setData('briqpay_psp_display_name', $pspDisplayName);
         } else {
             $this->logger->debug('PSP Display Name is empty.');
         }
-    
+
         // Set reservation ID
         if ($pspReservationId !== '') {
             $order->setData('briqpay_psp_reservationId', $pspReservationId);
         } else {
             $this->logger->debug('PSP Reservation ID is empty.');
         }
-    
+
         // Set session ID
         $sessionId = $sessionData['sessionId'] ?? '';
         if ($sessionId !== '') {
@@ -201,7 +201,7 @@ class AsyncCreateOrder
         } else {
             $this->logger->info('Session ID is empty.');
         }
-    
+
         // Construct backoffice URL
         $merchantId = '';
         if (isset($sessionData['clientToken'])) {
@@ -216,7 +216,7 @@ class AsyncCreateOrder
         } else {
             $this->logger->warning('Client token not found in session data.');
         }
-        
+
         if ($merchantId !== '' && $sessionId !== '') {
             $testmode = $this->scopeHelper->getScopedConfigValue('payment/briqpay/test_mode', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
             $backofficeUrl = 'https://app.briqpay.com/dashboard/sessions/orders/' . $sessionId . '?test=' . $testmode . '&merchantId=' . $merchantId;
@@ -228,23 +228,23 @@ class AsyncCreateOrder
         $order->setData('briqpay_psp_provider', $pspProviderName);
         $order->setData('briqpay_psp_underlying_payment_method', $underlyingPspName);
         $order->setData('briqpay_secondary_reservationId', $secondaryReference);
-    
+
         // Set session status
         if ($briqpaySessionStatus !== '') {
             $order->setData('briqpay_session_status', $briqpaySessionStatus);
         } else {
             $this->logger->warning('Briqpay Session Status is empty.');
         }
-    
+
         // Set business-specific data
         if ($sessionData['customerType'] === 'business' && isset($sessionData['data']['company'])) {
             $company = $sessionData['data']['company'];
             $companyCin = $company['cin'] ?? null;
             $companyName = $company['name'] ?? null;
             $companyVatno = $company['vatNumber'] ?? null;
-    
-           
-            
+
+
+
             if ($companyCin !== null) {
                 $order->setData('briqpay_cin', $companyCin);
             }
@@ -257,7 +257,7 @@ class AsyncCreateOrder
         } else {
             $this->logger->warning('Customer is not a business or company data not found.');
         }
-    
+
         // Set strongAuth data
         if (isset($sessionData['data']['strongAuth'])) {
             $strongAuth = $sessionData['data']['strongAuth'];
@@ -270,11 +270,11 @@ class AsyncCreateOrder
         } else {
             $this->logger->info('No strongAuth found.');
         }
-        
+
         // Log end of function
         $this->logger->info('Finished setOrderAdditionalData function.');
     }
-    
+
 
     protected function extractBillingEmail($quote, $sessionData)
     {
--- Briqpay/Payments/Setup/UpgradeSchema.php
+++ Briqpay/Payments/Setup/UpgradeSchema.php
@@ -22,8 +22,8 @@ class UpgradeSchema implements UpgradeSchemaInterface
         $setup->startSetup();
         $this->logger->info('UpgradeSchema: start');
 
-        if (version_compare($context->getVersion(), '1.1.10', '<')) {
-            $this->logger->info('UpgradeSchema: version is less than 1.1.10');
+        if (version_compare($context->getVersion(), '1.1.11', '<')) {
+            $this->logger->info('UpgradeSchema: version is less than 1.1.11');
 
             $this->createOrderTableColumns($setup);
             $this->createQuoteTableColumns($setup);
